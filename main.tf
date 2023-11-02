159ef480b3c50dba14b3173f55a0183fd90c88bf
Martyna Marcinkowska <martyna.marcinkowska@onet.pl>
2023-11-02 15:40:14 +0000

    comment out backend config

--- main.tf
+++ main.tf
@@ -7,10 +7,10 @@ terraform {
         }
     }
     backend "azurerm" {
-        resource_group_name   = "tfstate"
-        storage_account_name  = "synapsestatestorage"
-        container_name        = "tfstate"
-        key                   = "terraform.tfstate"
+        # resource_group_name   = "tfstate"
+        # storage_account_name  = "synapsestatestorage"
+        # container_name        = "tfstate"
+        # key                   = "terraform.tfstate"
     }
 }
 
